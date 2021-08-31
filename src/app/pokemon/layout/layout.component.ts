ace19a8427647008256327b9c5e3ed1f9bd1ff9c
Antonio Pérez <antperdev@gmail.com>
2021-08-31 13:03:44 +0200

    tabs implementado en layout pendiente

--- src/app/pokemon/layout/layout.component.ts
+++ src/app/pokemon/layout/layout.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// 1ª Forma:
+type Tabs = 'tab-favoritos' | 'tab-pokemons';
+// 2ª Forma: con enum
+// enum Tabs {
+//   tabFavoritos,
+//   tabPokemons
+// }
+
+
 
 @Component({
   selector: 'pokemon-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
 
-  constructor() { }
+  // 1ª Forma:
+  public currentTab: Tabs = 'tab-pokemons';
+  // 2ª Forma con enums
+  //  public currentTab: Tabs = Tabs.tabPokemons;
 
-  ngOnInit(): void {
-  }
 
+   constructor(){
+
+   }
 }
