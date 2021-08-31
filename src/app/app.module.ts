e240c726b4a8324a815ef85d0008d9b5e3ef41ff
Antonio Pérez <antperdev@gmail.com>
2021-08-31 11:46:50 +0200

    Modulo pokemon creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { PokemonModule } from './pokemon/pokemon.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PokemonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
